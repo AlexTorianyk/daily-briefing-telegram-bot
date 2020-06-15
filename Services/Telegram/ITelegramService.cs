57187876a05576e67d9cd23477f10942ec8e8d3a
Alex Toryanik <alex1toryanik@gmail.com>
2020-06-15 09:51:10 +0300

    feature: telegram services

---
+++ Services/Telegram/ITelegramService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace daily_briefing_telegram_bot.Services.Telegram
+{
+    public interface ITelegramService
+    {
+        Task SendMessage(string message);
+    }
+}
---
+++ Services/Telegram/TelegramService.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using daily_briefing_telegram_bot.Extensions.AutomaticDependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+
+namespace daily_briefing_telegram_bot.Services.Telegram
+{
+    public class TelegramService : ITelegramService, IScoped
+    {
+        private readonly long ChatId;
+        private readonly string BotToken;
+
+        public TelegramService(IConfiguration configuration)
+        {
+            ChatId = configuration.GetSection("Telegram").GetValue<int>("ChatId");
+            BotToken = configuration.GetSection("Telegram").GetValue<string>("BotToken");
+        }
+
+        public async Task SendMessage(string message)
+        {
+            var botClient = new TelegramBotClient(BotToken);
+            await botClient.SendTextMessageAsync(ChatId, message);
+        }
+    }
+}
